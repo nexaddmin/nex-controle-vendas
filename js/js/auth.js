ea10611386ea44187e74b8f01c6cffe1ac2fc9a6
nexaddmin <nexfinanceiroaddmin@gmail.com>
2026-02-01 17:42:49 -0300

    auth.js

--- js/js/auth.js
+++ js/js/auth.js
@@ -1,3 +1,17 @@
 function login() {
-  alert("Login funcionando! Próximo passo: conectar ao sistema.");
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+
+    if (email === "" || senha === "") {
+        alert("Preencha email e senha");
+        return;
+    }
+
+    // LOGIN DE TESTE
+    if (email === "admin@nex.com" && senha === "123456") {
+        alert("Login realizado com sucesso!");
+        // futuramente: window.location.href = "admin.html";
+    } else {
+        alert("Email ou senha inválidos");
+    }
 }
