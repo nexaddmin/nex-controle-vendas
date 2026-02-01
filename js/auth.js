168d4338b29d44355083113dd6cbfd2537ee775e
nexaddmin <nexfinanceiroaddmin@gmail.com>
2026-02-01 18:10:18 -0300

    auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,11 @@
+function login() {
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
 
+    if (!email || !senha) {
+        alert("Preencha email e senha");
+        return;
+    }
+
+    alert("Login clicado com sucesso!\nEmail: " + email);
+}
