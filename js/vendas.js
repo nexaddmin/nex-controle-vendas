d7be7eb16f69417f381d7e6129037f98fb5e53ae
nexaddmin <nexfinanceiroaddmin@gmail.com>
2026-02-08 11:05:34 -0300

    vendas.js

--- js/vendas.js
+++ js/vendas.js
@@ -1,1 +1,10 @@
-console.log("vendas.js carregado");
+// js/cliente.js
+
+const tipo = localStorage.getItem("tipoUsuario");
+
+if (tipo !== "cliente") {
+  alert("Acesso não autorizado");
+  window.location.href = "index.html";
+} else {
+  console.log("Cliente autorizado");
+}
