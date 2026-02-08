18b83340a949a61aca08ced86cffc7bacd692ee0
nexaddmin <nexfinanceiroaddmin@gmail.com>
2026-02-08 10:50:49 -0300

    auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,25 +1,32 @@
-function login() {
+// js/auth.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("loginForm");
+
+  if (!form) return;
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
     const usuario = document.getElementById("usuario").value;
     const senha = document.getElementById("senha").value;
 
-    if (!usuario || !senha) {
-        alert("Informe usuário e senha");
-        return;
-    }
-
-    // ADMIN
+    // LOGIN ADMIN
     if (usuario === "admin" && senha === "1234") {
-        localStorage.setItem("tipoUsuario", "admin");
-        window.location.href = "admin.html";
-        return;
+      localStorage.setItem("usuarioLogado", "admin");
+      localStorage.setItem("tipoUsuario", "admin");
+      window.location.href = "admin.html";
+      return;
     }
 
-    // CLIENTE
-    if (senha === "1234") {
-        localStorage.setItem("tipoUsuario", "cliente");
-        window.location.href = "dashboard.html";
-        return;
+    // LOGIN CLIENTE
+    if (usuario === "cliente" && senha === "1234") {
+      localStorage.setItem("usuarioLogado", "cliente");
+      localStorage.setItem("tipoUsuario", "cliente");
+      window.location.href = "cliente.html";
+      return;
     }
 
     alert("Usuário ou senha inválidos");
-}
+  });
+});
